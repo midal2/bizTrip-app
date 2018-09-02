cfaf829cb309b3ab0ddb1528ade76075a1a739ee
midal2 <midal2@midal2ui-MacBook-Pro.local>
2018-09-02 09:31:06 +0900

    웹뷰추가

--- app/src/main/java/biztrip/dz/app/stocktest/MainActivity.java
+++ app/src/main/java/biztrip/dz/app/stocktest/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         final MyFragment myFragment = new MyFragment();
         pageAdapter.addFragement(myFragment, "첫번째");
-        pageAdapter.addFragement(new MyFragment(), "두번째");
+        pageAdapter.addFragement(new WebViewFragment(), "두번째");
         pageAdapter.addFragement(new MyFragment(), "세번째");
         pageAdapter.addFragement(new MyFragment(), "네번째");
         viewPager.setAdapter(pageAdapter);
@@ -54,36 +54,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        /*RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        stockAdapter = new StockAdapter();
-
-        recyclerView.setAdapter(stockAdapter);
-
-        Button button =  findViewById(R.id.button);
-        button.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeDataSet();
-            }
-
-            private void changeDataSet() {
-                List<StockAdapter.StockInfo> stockInfoList = new LinkedList<>();
-                StockAdapter.StockInfo stockInfo = new StockAdapter.StockInfo();
-                stockInfo.col1 = "t1";
-                stockInfo.col2 = "t2";
-                stockInfo.col3 = "t3";
-                stockInfoList.add(stockInfo);
-
-                stockInfo = new StockAdapter.StockInfo();
-                stockInfo.col1 = "t4";
-                stockInfo.col2 = "t5";
-                stockInfo.col3 = "t6";
-                stockInfoList.add(stockInfo);
-
-                stockAdapter.setStockInfoList(stockInfoList);
-                stockAdapter.notifyDataSetChanged();
-            }
-        });*/
     }
 }
